1ea13e1f8e741f6f3295ff5ded2b805c3a034940
Gilberto Falco <gilberto.falco@outlook.com>
2022-09-01 16:13:43 -0300

    Node API correction

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ app.post('/create', async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => { console.log(`Server is running on PORT ${PORT}`) })
+app.listen(PORT)
